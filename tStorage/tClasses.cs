a321d0f9b610b1155e9acbd765ea212dc95188ec
stas-github-projects <thatsmytempmail@gmail.com>
2016-07-06 12:15:13 +0300

    add classes

---
+++ tStorage/tClasses.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tStorage
+{
+    public partial class tEngine
+    {
+        public class tTree
+        {
+            private tGlobals _globals;
+            public tTree(tGlobals _globals)
+            { this._globals = _globals; }
+            
+        }
+    }
+
+}
